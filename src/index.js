e99161af00fd0977132627f6b6a049fd5e3f87e1
Yasir <Yasir.A.Kulane@gmail.com>
2020-10-07 14:26:56 -0500

    fixed stuffz

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Preview from './preview/Preview';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Preview />
   </React.StrictMode>,
   document.getElementById('root')
 );
