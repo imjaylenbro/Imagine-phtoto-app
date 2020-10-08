b144e435a8d1a180ae17198e6547e9be4a3d7b98
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-10-08 14:00:05 -0400

    moved preview component from index.js to app.js and added app.js to index.js so all components show in browser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Filter from "./filter/filter.js";
 import Gallery from "../src/gallery/Gallery";
+import Preview from "./preview/Preview";
 
 function App() {
   return (
     <div className="App">
       <Gallery />
-      <Filter></Filter>
+      <Filter />
+      <Preview />
     </div>
   );
 }
