4dd7268e737c9d3d84054213dbebdf8ccba65799
imjaylenbro <55603069+imjaylenbro@users.noreply.github.com>
2020-10-09 04:23:18 -0700

    adding jsx styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-// import Preview from "./preview/Preview";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Share from './share/share'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Share/>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
